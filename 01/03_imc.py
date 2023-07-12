85f446423a40180cd96774b7c54abe519622ad92
Daniel Bonilla <87622543+Nakajito@users.noreply.github.com>
2023-07-12 00:49:31 +0000

    ejercicio que captura el peso y estatura para calcular el IMC

--- 01/03_imc.py
+++
@@ -1,4 +0,0 @@
-peso = int(input("Introduce tu peso en kg: "))
-estatura = float(input('Introduce tu estatura en m: '))
-imc = peso / (estatura * estatura)
-print(f'Tu índice de masa corporal es {imc:.2f}')
